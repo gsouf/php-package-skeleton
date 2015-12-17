7c7a636a79f562d2beeb982fe5142cd90b824197
Soufiane Ghzal <sghzal@gmail.com>
2015-12-17 14:01:39 +0100

    Update phpcs.bash

--- test/bin/phpcs.bash
+++ test/bin/phpcs.bash
@@ -5,5 +5,5 @@ SCRIPTDIR=$(dirname "$SCRIPTFILE")
 
 
 if [ -n "$1" ]; then report=$1; else report="summary"; fi
+cd $SCRIPTDIR/../.. && $SCRIPTDIR/../../vendor/bin/phpcs --standard="$SCRIPTDIR/../../phpcs.xml" --report=$report -s
 
-cd $SCRIPTDIR/../.. && $SCRIPTDIR/../../vendor/bin/phpcbf --standard="$SCRIPTDIR/phpcs.xml"
