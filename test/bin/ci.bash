71419d97d89da84d2fd3a799dc55caa0334daa9e
Soufiane Ghzal <sghzal@gmail.com>
2016-01-10 17:03:10 +0100

    Update ci.bash

--- test/bin/ci.bash
+++ test/bin/ci.bash
@@ -30,22 +30,18 @@ echo -e "=================================\e[39m"
 
 $SCRIPTDIR/phpcs.bash $1
 
-echo "OK"
+echo "Code standards: OK"
 
 
+echo -e "\e[34m"
+echo "============================"
+echo -e "= \e[1m\e[33mResporting code coverage\e[0m\e[34m ="
+echo -e "============================\e[39m"
 if [ "$PROCESS_CODECLIMATE" = true ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]
 then
-
     composer require codeclimate/php-test-reporter:dev-master
-
-    echo -e "\e[34m"
-    echo "============================"
-    echo -e "= \e[1m\e[33mRepporting code coverage\e[0m\e[34m ="
-    echo -e "============================\e[39m"
-
     ./vendor/bin/test-reporter
 else
-    echo ""
     echo "Skip code coverage report..."
 fi
 
