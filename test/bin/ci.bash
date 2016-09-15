9ca1a41bb0ea1662f661c884b29df04f9d254b50
Soufiane Ghzal <sghzal@gmail.com>
2016-09-15 23:54:44 +0200

    Update ci.bash

--- test/bin/ci.bash
+++ test/bin/ci.bash
@@ -11,7 +11,7 @@ echo "======================"
 echo -e "= \e[1m\e[33mRunning unit tests\e[0m\e[34m ="
 echo -e "======================\e[39m"
 
-phpunit -c "$SCRIPTDIR/../../phpunit.dist.xml"
+composer test
 
 
 echo -e "\e[34m"
